815f3841a0fe46deb03e691c393cacb5868f5ee2
Stasnazarenko <Stasnazarenko46@gmail.com>
2025-12-08 11:23:44 +0100

    Convert utils.js from CommonJS to ES6 import/export syntax

--- utils.js
+++ utils.js
@@ -1,6 +1,5 @@
-const figlet = require('figlet');
-const db = require('./scr/db');
-
+import figlet from 'figlet';
+import * as db from './scr/db.js';
 
 let storageType;
 let engine = 'main';
@@ -13,7 +12,7 @@ function printLogo(){
     width: 200,
     whitespaceBreak: true
   }));
-};
+}
 
 function menu(){
   let arr = ['1. New profile'];
@@ -21,27 +20,19 @@ function menu(){
   for (let profile of db.get_Profiles()){
     i++;
     arr.push(`${i}. ${profile}`);
-  };
+  }
   return arr;
-};
+}
 
 function timeLog(){
   let now = new Date();
   let hours = now.getHours();
   let minutes = now.getMinutes();
   let seconds = now.getSeconds();
-  if (hours < 10)
-    hours = `0${hours}`;
-  if (minutes < 10)
-    minutes = `0${minutes}`;
-  if (seconds < 10)
-    seconds = `0${seconds}`;
+  if (hours < 10) hours = `0${hours}`;
+  if (minutes < 10) minutes = `0${minutes}`;
+  if (seconds < 10) seconds = `0${seconds}`;
   return `${hours}:${minutes}:${seconds} >>> `;
-};
+}
 
-
-module.exports.printLogo = printLogo;
-module.exports.menu = menu;
-module.exports.timeLog = timeLog;
-module.exports.storageType = storageType;
-module.exports.engine = engine;
+export { printLogo, menu, timeLog, storageType, engine };
