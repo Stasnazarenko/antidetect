735e6e73b18d32fe8531f0b0bd624541033719a8
Stasnazarenko <Stasnazarenko46@gmail.com>
2025-12-08 11:26:24 +0100

    Convert config.js from CommonJS to ES6 import/export syntax

--- config.js
+++ config.js
@@ -1,19 +1,11 @@
-require('dotenv').config();
+import 'dotenv/config.js';
 
 const cloudDir = process.env.DIR;
-const storageDir = __dirname + '/storage/';
+const storageDir = process.cwd() + '/storage/';
 const tags = ['Email', 'Gmail', 'Twitter', 'Metamask', 'Phantom', 'Discord'];
 const googleEmail = process.env.GOOGLEEMAIL;
 const googleKey = process.env.GOOGLEKEY;
 const googleSheetID = process.env.GOOGLESHEETID;
-
-// Check if Google Sheets is configured
 const useGoogleSheets = !!(googleEmail && googleKey && googleSheetID);
 
-module.exports.cloudDir = cloudDir;
-module.exports.storageDir = storageDir;
-module.exports.tags = tags;
-module.exports.googleEmail = googleEmail;
-module.exports.googleKey = googleKey;
-module.exports.googleSheetID = googleSheetID;
-module.exports.useGoogleSheets = useGoogleSheets;
+export { cloudDir, storageDir, tags, googleEmail, googleKey, googleSheetID, useGoogleSheets };
